fba95b402b8393172fa3f01c482bc7c3b5e30b31
Mansor Almossa <malmossa@gmail.com>
2021-11-04 12:25:36 -0700

    Completed

--- js/main.js
+++ js/main.js
@@ -1,3 +1,17 @@
-function HelloWorld() {
-  alert("Hello World!");
+let fizzNumber;
+let buzzNumber;
+
+document.querySelector(".btn").addEventListener("click", getValue);
+
+// Get User Input
+function getValue(event) {
+  fizzNumber = document.getElementById("fizz-value").value;
+  buzzNumber = document.getElementById("buzz-value").value;
+  console.log(fizzNumber);
+  console.log(buzzNumber);
+
+  event.preventDefault();
 }
+
+// Fizz Buzz Logic
+function fizzBuzz() {}
